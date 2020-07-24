1be1c79872b0cf39c00d322943adc1d2415acbdd
Temilola Abikoye <temilolaabikoye@Temilolas-MacBook-Air.local>
2020-07-24 17:14:43 +0100

    Updates

---
+++ precache-manifest.d6533b172d25259e67eecdffa8483d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3089a97168ad586905407b39e2c590e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "671a912f1b4817e68433",
+    "url": "/monsters-rolodex/static/css/main.7b71c8a7.chunk.css"
+  },
+  {
+    "revision": "ed6170eebe8dd6d5fb93",
+    "url": "/monsters-rolodex/static/js/2.428a0564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.428a0564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671a912f1b4817e68433",
+    "url": "/monsters-rolodex/static/js/main.18704d56.chunk.js"
+  },
+  {
+    "revision": "e4964afe8bbd3a048590",
+    "url": "/monsters-rolodex/static/js/runtime-main.17a0d17b.js"
+  }
+]);
